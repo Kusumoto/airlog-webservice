5b3d3ae187280f1d4b300bc4ab9c083fddc91f5c
Weerayut Hongsa <kusumoto.com@gmail.com>
2016-02-25 02:11:10 +0700

    Fix Access Token Checker Issus.

--- src/controllers/application_api.js
+++ src/controllers/application_api.js
@@ -10,7 +10,7 @@ module.exports = [{
     method: 'POST',
     path: '/api/applications',
     handler: function (request, reply) {
-        api.findOne({ 'api_key': request.payload.access_token }, function (err, api) {
+        api.findOne({ 'api_key': request.payload.access_token, 'api_isenable': true }, function (err, api) {
             if (err) throw err;
             if (api != undefined || api != null) {
                 applications.find({}, 'application_name application_lang application_agent', function (err, apps) {
@@ -33,7 +33,7 @@ module.exports = [{
         method: 'POST',
         path: '/api/application/{app_id}',
         handler: function (request, reply) {
-            api.findOne({ 'api_key': request.payload.access_token }, function (err, api) {
+            api.findOne({ 'api_key': request.payload.access_token, 'api_isenable': true }, function (err, api) {
                 if (err) throw err;
                 if (api != undefined || api != null) {
                     applications.findOne({ '_id': request.params.app_id }, 'application_name application_lang application_agent', function (err, apps) {
@@ -59,7 +59,7 @@ module.exports = [{
         method: 'POST',
         path: '/api/application/{app_id}/listfunction',
         handler: function (request, reply) {
-            api.findOne({ 'api_key': request.payload.access_token }, function (err, api) {
+            api.findOne({ 'api_key': request.payload.access_token, 'api_isenable': true }, function (err, api) {
                 if (err) throw err;
                 if (api != undefined || api != null) {
                     functions.find({ 'application_id': request.params.app_id }, 'function_name application_id application_name function_primary', function (err, func) {
@@ -84,7 +84,7 @@ module.exports = [{
         method: 'POST',
         path: '/api/application/{app_id}/loggercount',
         handler: function (request, reply) {
-            api.findOne({ 'api_key': request.payload.access_token }, function (err, api) {
+            api.findOne({ 'api_key': request.payload.access_token, 'api_isenable': true }, function (err, api) {
                 if (err) throw err;
                 if (api != undefined || api != null) {
                     applications.findOne({ '_id': request.params.app_id }, function (err, app) {
@@ -139,28 +139,30 @@ module.exports = [{
         method: 'POST',
         path: '/api/application/{app_id}/usercount',
         handler: function (request, reply) {
-            if (api != undefined || api != null) {
-                applications.findOne({ '_id': request.params.app_id }, function (err, app) {
-                    if (err) throw err;
-                    if (app != undefined || app != null) {
-                        if (request.payload.start == undefined || request.payload.start == null || request.payload.end == undefined || request.payload.end == null) {
-                            useds.count({ 'use_appid': request.params.app_id }, function (err, count) {
-                                console.log('[%s] : access_token = %s : call /api/application/%s/usercount', new Date(), request.payload.access_token, request.params.app_id);
-                                reply({ 'statusCode': 200, 'application_id': app._id, 'application_name': app.application_name, 'data': { 'used': count } });
-                            });
+            api.findOne({ 'api_key': request.payload.access_token, 'api_isenable': true }, function (err, api) {
+                if (api != undefined || api != null) {
+                    applications.findOne({ '_id': request.params.app_id }, function (err, app) {
+                        if (err) throw err;
+                        if (app != undefined || app != null) {
+                            if (request.payload.start == undefined || request.payload.start == null || request.payload.end == undefined || request.payload.end == null) {
+                                useds.count({ 'use_appid': request.params.app_id }, function (err, count) {
+                                    console.log('[%s] : access_token = %s : call /api/application/%s/usercount', new Date(), request.payload.access_token, request.params.app_id);
+                                    reply({ 'statusCode': 200, 'application_id': app._id, 'application_name': app.application_name, 'data': { 'used': count } });
+                                });
+                            } else {
+                                useds.count({ 'use_appid': request.params.app_id, 'used_datetime': { "$gte": new Date(request.payload.start), "$lt": new Date(request.payload.end) } }, function (err, count) {
+                                    console.log('[%s] : access_token = %s : call /api/application/%s/usercount', new Date(), request.payload.access_token, request.params.app_id);
+                                    reply({ 'statusCode': 200, 'application_id': app._id, 'application_name': app.application_name, 'data': { 'used': count } });
+                                });
+                            }
                         } else {
-                            useds.count({ 'use_appid': request.params.app_id, 'used_datetime': { "$gte": new Date(request.payload.start), "$lt": new Date(request.payload.end) } }, function (err, count) {
-                                console.log('[%s] : access_token = %s : call /api/application/%s/usercount', new Date(), request.payload.access_token, request.params.app_id);
-                                reply({ 'statusCode': 200, 'application_id': app._id, 'application_name': app.application_name, 'data': { 'used': count } });
-                            });
+                            reply({ 'statusCode': '400', 'error': 'Invalid application id.' });
                         }
-                    } else {
-                        reply({ 'statusCode': '400', 'error': 'Invalid application id.' });
-                    }
-                });
-            } else {
-                reply({ 'statusCode': '400', 'error': 'Invalid Access token.' });
-            }
+                    });
+                } else {
+                    reply({ 'statusCode': '400', 'error': 'Invalid Access token.' });
+                }
+            });
         }, config: {
             validate: {
                 payload: {
