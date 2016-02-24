847e8a54b7e655bba2a9ce08b2905d63663ccd99
Weerayut Hongsa <kusumoto.com@gmail.com>
2016-02-25 03:34:04 +0700

    Fix Mongoose data type.

--- src/models/api_model.js
+++ src/models/api_model.js
@@ -3,7 +3,7 @@ var Mongoose = require('../database').Mongoose;
 var apiSchema = new Mongoose.Schema({
     api_key : String,
     api_name : String,
-    api_isenable : String
+    api_isenable : Boolean
 }, { collection : 'api' });
 
 exports.API = Mongoose.model('api',apiSchema);
